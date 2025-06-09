32cd3ba21df5c0f52046aee69e7db44c989807ca
justinproject1115 <88981164+justinproject1115@users.noreply.github.com>
2025-06-10 00:57:57 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,32 +2,28 @@
 
 WALLET="RQAJNrnHHrUKWnfm3axM4CFtnFdhtBPo6b"
 
-# Download cpuminer-opt if not already present
-if [ ! -f "./cpuminer" ]; then
-    echo "Downloading cpuminer-opt binary..."
-    sudo apt update && sudo apt install -y git build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev &&
-    git clone https://github.com/JayDDee/cpuminer-opt.git &&
-    cd cpuminer-opt
-    ./build.sh
-fi
-
-# Infinite mining loop with algo switching
 while true; do
     echo "Mining minotaurx..."
     ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.mine.zpool.ca:7019 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 600
+    sleep 60
     kill $PID
 
     echo "Mining X11..."
     ./cpuminer -a x11 -o stratum+tcp://x11.mine.zpool.ca:3533 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 600
+    sleep 60
     kill $PID
 
     echo "Mining Yescrypt..."
     ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 600
+    sleep 60
+    kill $PID
+
+    echo "Mining Yespower..."
+    ./cpuminer-opt -a yespower -o stratum+tcp://yespower.mine.zpool.ca:6234 -u $WALLET -p c=RVN &
+    PID=$!
+    sleep 60
     kill $PID
 done
