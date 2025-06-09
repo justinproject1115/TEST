8c33b934e806a0bfea4ca2d926c3f6199ee07de4
justinproject1115 <88981164+justinproject1115@users.noreply.github.com>
2025-06-10 00:59:53 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -2,6 +2,15 @@
 
 WALLET="RQAJNrnHHrUKWnfm3axM4CFtnFdhtBPo6b"
 
+# Download cpuminer-opt if not already present
+if [ ! -f "./cpuminer" ]; then
+    echo "Downloading cpuminer-opt binary..."
+    sudo apt update && sudo apt install -y git build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev &&
+    git clone https://github.com/JayDDee/cpuminer-opt.git &&
+    cd cpuminer-opt
+    ./build.sh
+fi
+
 while true; do
     echo "Mining minotaurx..."
     ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.mine.zpool.ca:7019 -u $WALLET -p c=RVN &
