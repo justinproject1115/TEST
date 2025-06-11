4b3b6c96a36b4eaf58670bd7bd8a3438d9e7a572
justinproject1115 <88981164+justinproject1115@users.noreply.github.com>
2025-06-11 20:16:30 +0800

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -15,24 +15,18 @@ while true; do
     echo "Mining minotaurx..."
     ./cpuminer -a minotaurx -o stratum+tcp://minotaurx.mine.zpool.ca:7019 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 60
-    kill $PID
-
-    echo "Mining X11..."
-    ./cpuminer -a x11 -o stratum+tcp://x11.mine.zpool.ca:3533 -u $WALLET -p c=RVN &
-    PID=$!
-    sleep 60
+    sleep 300
     kill $PID
 
     echo "Mining Yescrypt..."
     ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.mine.zpool.ca:6233 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 60
+    sleep 300
     kill $PID
 
     echo "Mining Yespower..."
     ./cpuminer -a yespower -o stratum+tcp://yespower.mine.zpool.ca:6234 -u $WALLET -p c=RVN &
     PID=$!
-    sleep 60
+    sleep 300
     kill $PID
 done
